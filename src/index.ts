63071980d7cee52853efe464513c95b538805071
katherine <katerynaunik@gmail.com>
2026-09-07 13:32:37 +0300

    next

--- src/index.ts
+++ src/index.ts
@@ -48,10 +48,10 @@
 // a = 10
 // let a2:unknown 
 
-import * as fs from "node:fs"
-import path from "node:path"
+import * as fs from "node:fs" // підключення модуля файлової системи
+import path from "node:path"  // модуль для роботи зі шляхами
 import process from "node:process"
-//import (stdout, stdin) from "node:process"
+import {stdout, stdin} from "node:process"
 
 const FILE_TO_PATH = path.join('logs', 'logs.txt');
 // fs.writeFileSync(FILE_TO_PATH, "Node");
@@ -61,12 +61,20 @@ const FILE_TO_PATH = path.join('logs', 'logs.txt');
 
 async function writeToFile(filePath:string, content:string):Promise<void>{
     try{
-        await fs.writeFile(filePath, content+'\n', 'utf-8')
-        console.log("Файл успішно збережено")
+        await fs.appendFile(filePath, content+'\n', 'utf-8')
+        // await fs.writeFile(filePath, content+'\n', 'utf-8')
+        stdout.write("Файл успішно збережено")
     }
     catch(error){
         console.log("Файл не збережено")
     }
 }
 
-// 
+stdin.on('data', (data:Buffer) => {
+    console.log("Bytes", data);
+    const content:string = data.toString('utf-8');
+    console.log("Content", content);
+    writeToFile(FILE_TO_PATH, content).then(_=> {
+        
+    })
+})
