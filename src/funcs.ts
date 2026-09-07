ab01876c5260905f05573b772d203a15211f5153
katherine <katerynaunik@gmail.com>
2026-09-07 14:57:38 +0300

    read method

--- src/funcs.ts
+++ src/funcs.ts
@@ -1,35 +1,86 @@
-// * as readline - аліас
-import * as fs from "node:fs/promises" // підключення модуля файлової системи
-import path from "node:path"  // модуль для роботи зі шляхами
-import process from "node:process"
-import {stdout as output, stdin as input} from "node:process" // ввід-вивід
-import * as readline from "node:readline/promises" // модуль для зручного читання построково
+// // * as readline - аліас
+// import * as fs from "node:fs/promises" // підключення модуля файлової системи
+// import path from "node:path"  // модуль для роботи зі шляхами
+// import process from "node:process"
+// import {stdout as output, stdin as input} from "node:process" // ввід-вивід
+// import * as readline from "node:readline/promises" // модуль для зручного читання построково
 
-// async означає асинхронну роботу та дозволяє використовувати await
-// await означає, що треба дочекатися, поки Promise завершиться і тільки після цього можна продовжити виконання async функції
+// // async означає асинхронну роботу та дозволяє використовувати await
+// // await означає, що треба дочекатися, поки Promise завершиться і тільки після цього можна продовжити виконання async функції
 
-async function getContent():Promise<string> {
-    const rl = readline.createInterface({input, output});
-    try {
-        const message:string = await rl.question("Enter your content: ");
-        return message;
-    } 
-    catch(error) {
-        console.log(`NO DATA ${error}`);
-        return "";
+// export async function getContent():Promise<string> {
+//     const rl = readline.createInterface({input, output});
+//     try {
+//         const message:string = await rl.question("Enter your content: ");
+//         return message;
+//     } 
+//     catch(error) {
+//         console.log(`NO DATA ${error}`);
+//         return "";
+//     }
+//     finally{
+//         rl.close();
+//     }
+// }
+
+// export async function writeToFile(filePath:string, content:string):Promise<void>{
+//     try{
+//         await fs.appendFile(filePath, content+'\n', 'utf-8') // додає текст в кінець файлу
+//         // await fs.writeFile(filePath, content+'\n', 'utf-8')
+//         input.write("Файл успішно збережено")
+//     }
+//     catch(error){
+//         console.error("Файл не збережено")
+//     }
+// }
+import * as fs from "node:fs/promises"
+import * as readline from "node:readline/promises"
+import {stdout as output, stdin as input } from "node:process"
+ 
+export default class FileWorker
+{
+    private static path_to_file:string;
+
+    public static set path(path:string){
+        FileWorker.path_to_file = path;
+    }
+
+    public static async getContent():Promise<string>{
+    const rl = readline.createInterface({input,output})
+    try{
+        const content:string =  await rl.question("Enter your content: ")
+        return content
+ 
+    }catch(error){
+        console.log(`no data ${error}`)
+        return ''
     }
     finally{
-        rl.close();
+        rl.close()
     }
 }
 
-async function writeToFile(filePath:string, content:string):Promise<void>{
+public static async writeToFile(filePath:string, content:string):Promise<void>{
     try{
-        await fs.appendFile(filePath, content+'\n', 'utf-8') // додає текст в кінець файлу
-        // await fs.writeFile(filePath, content+'\n', 'utf-8')
-        input.write("Файл успішно збережено")
+        await fs.appendFile(filePath, content+'\n', 'utf-8')
+        //await fs.writeFile(filePath, content+'\n', 'utf-8')
+        console.log("Файл успішно збережено")
     }
     catch(error){
-        console.error("Файл не збережено")
+        console.log("Файл не збережено")
     }
 }
+ 
+public static async readFile(filePath:string) {
+    try {
+        return await fs.readFile(filePath);
+        // const data:string = await fs.readFile(filePath, 'utf-8');
+        // console.log(data);
+    }
+    catch(error) {
+        console.error(`ERROR: ${error}`);
+    }
+}
+}
+
+
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
-import path from "node:path"  // модуль для роботи зі шляхами
-import {getContent, writeToFile} from "./funcs.js"
-const FILE_TO_PATH = path.join('logs', 'logs.txt');
+// import path from "node:path"  // модуль для роботи зі шляхами
+// import {getContent, writeToFile} from "./funcs.js"
+// const FILE_TO_PATH = path.join('logs', 'logs.txt');
 
 
-const content = await getContent();
-await writeToFile(FILE_TO_PATH, content);
+// const content = await getContent();
+// await writeToFile(FILE_TO_PATH, content);
+
+import path from "node:path"
+import FileWorker from "./funcs.js"
+const FILE_TO_PATH = path.join('logs','logs.txt')
+ 
+FileWorker.path = FILE_TO_PATH;
+let content:string|undefined = await FileWorker.getContent()
+await FileWorker.writeToFile(FILE_TO_PATH, content)
+content = (await FileWorker.readFile(FILE_TO_PATH))?.toString('utf-8');
+console.log(`Content from file ${content}`);
