add643fc6e3eb518bbcf3c973057801b93428d9c
katherine <katerynaunik@gmail.com>
2026-09-07 14:20:01 +0300

    next

--- src/index.ts
+++ src/index.ts
@@ -48,10 +48,12 @@
 // a = 10
 // let a2:unknown 
 
-import * as fs from "node:fs" // підключення модуля файлової системи
+// * as readline - аліас
+import * as fs from "node:fs/promises" // підключення модуля файлової системи
 import path from "node:path"  // модуль для роботи зі шляхами
 import process from "node:process"
-import {stdout, stdin} from "node:process" // ввід-вивід
+import {stdout as output, stdin as input} from "node:process" // ввід-вивід
+import * as readline from "node:readline/promises" // модуль для зручного читання построково
 
 const FILE_TO_PATH = path.join('logs', 'logs.txt');
 // fs.writeFileSync(FILE_TO_PATH, "Node");
@@ -61,22 +63,42 @@ const FILE_TO_PATH = path.join('logs', 'logs.txt');
 
 // async означає асинхронну роботу та дозволяє використовувати await
 // await означає, що треба дочекатися, поки Promise завершиться і тільки після цього можна продовжити виконання async функції
+
+async function getContent():Promise<void> {
+    const rl = readline.createInterface({input, output});
+    try {
+        const message:string = await rl.question("Enter your content: ");
+        console.log(`Content: ${message}`);
+    } 
+    catch(error) {
+        console.log("no data");
+        rl.close();
+    }
+    finally{
+        rl.close();
+    }
+}
+
 async function writeToFile(filePath:string, content:string):Promise<void>{
     try{
         await fs.appendFile(filePath, content+'\n', 'utf-8') // додає текст в кінець файлу
         // await fs.writeFile(filePath, content+'\n', 'utf-8')
-        stdout.write("Файл успішно збережено")
+        input.write("Файл успішно збережено")
     }
     catch(error){
         console.error("Файл не збережено")
     }
 }
 
-stdin.on('data', (data:Buffer) => {
-    console.log("Bytes", data);
-    const content:string = data.toString('utf-8');
-    console.log("Content", content);
-    writeToFile(FILE_TO_PATH, content).then(_=> {
-
-    })
+getContent().then((data:string) => {
+    writeToFile(FILE_TO_PATH, data);
 })
+
+// stdin.on('data', (data:Buffer) => {
+//     console.log("Bytes", data);
+//     const content:string = data.toString('utf-8');
+//     console.log("Content", content);
+//     writeToFile(FILE_TO_PATH, content).then(_=> {
+
+//     })
+// })
