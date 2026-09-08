1ddc56fc818f6737e886ea293a3f0b8c7f0c1eb1
Katherine <katerynaunik@gmail.com>
2026-09-08 15:37:22 +0300

    ok

--- src/index.ts
+++ src/index.ts
@@ -19,13 +19,13 @@ try {
 catch(error) {
     console.log("Error creating folder: ", error);
 }
+
 const FILE_TO_PATH = path.join(FOLDER_NAME,'logs.txt')
 
-
-
- 
 FileWorker.path = FILE_TO_PATH;
+
 let content:string|undefined = await FileWorker.getContent()
 await FileWorker.writeToFile(FILE_TO_PATH, content)
 content = (await FileWorker.readFile(FILE_TO_PATH))?.toString('utf-8');
 console.log(`Content from file: ${content} \n`);
+
